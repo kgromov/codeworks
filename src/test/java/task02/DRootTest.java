4e33aaa16c3d6f6495deae80e37a52faabb44ab6
Konstantin <konstantin@revimedia.com>
2020-03-10 21:38:52 +0200

    check upstreaming

--- src/test/java/task02/DRootTest.java
+++ src/test/java/task02/DRootTest.java
@@ -12,6 +12,7 @@ public class DRootTest {
     public void Tests() {
         assertEquals( "Nope!" , 7, DRoot.digital_root(16));
         assertEquals( "Nope!" , 6, DRoot.digital_root(456));
+        assertEquals( "Nope!" , 6, DRoot.digital_root(411));
     }
 
 }
